adeb5fa99aa0b1c49a25fa96222ad831caa819d3
layne nigro <laynenigro@gmail.com>
2023-01-26 15:03:13 -0500

    added skill and form cards

--- src/component/MainCard.js
+++ src/component/MainCard.js
@@ -32,6 +32,85 @@ function MainCard() {
     > <h1>My name is Layne Nigro and I am a highly motivated fullstack web developer. I have a passion for functional design and I love working with computers.</h1>
 </Typography>
     </Box>
+    <Box 
+    border={1} borderColor="white" borderRadius="10px"
+    display="flex" justifyContent="center" alignItems="center"
+    fontSize={24}
+      sx={{
+        width: '100',
+        marginTop: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        height: 500,
+        backgroundColor: '#2E3B55',
+        '&:hover': {
+          backgroundColor: '#2E3B55',
+          fontSize:26,
+          boxShadow: 5,
+        },
+        
+      }}
+    >
+<Typography class= "neonText"
+    color= "White"
+    marginBottom={30}
+    > <h1>This is my project section</h1>
+</Typography>
+    </Box>
+    <div class= "skillForm">
+    <Box 
+    border={1} borderColor="white" borderRadius="10px"
+    display="flex" justifyContent="center" alignItems="center"
+    fontSize={24}
+      sx={{
+        width: '50%',
+        marginTop: 1,
+        marginLeft: 5,
+        marginRight: 1,
+        height: 400,
+        backgroundColor: '#2E3B55',
+        '&:hover': {
+          backgroundColor: '#2E3B55',
+          fontSize:26,
+          boxShadow: 5,
+        },
+        
+      }}
+    >
+<Typography class= "neonText"
+    color= "White"
+    marginBottom={30}
+    > <h1>This is my skills section</h1>
+</Typography>
+    </Box>
+    <Box 
+    border={1} borderColor="white" borderRadius="10px"
+    display="flex" justifyContent="center" alignItems="center"
+    fontSize={24}
+      sx={{
+        width: '50%',
+        marginTop: 1,
+        marginLeft: 1,
+        marginRight: 5,
+        height: 400,
+        backgroundColor: '#2E3B55',
+        float:'right',
+        displayFlex: 'inline',
+        '&:hover': {
+          backgroundColor: '#2E3B55',
+          fontSize:26,
+          boxShadow: 5,
+        },
+        
+      }}
+    >
+<Typography class= "neonText"
+    color= "White"
+    marginBottom={30}
+    > <h1>This is my form section</h1>
+</Typography>
+    </Box>
+    </div>
       </>
     );
   }
