f6a7c657671c74c834a32b5a6e5a272c5069130e
layne nigro <laynenigro@gmail.com>
2023-01-26 00:18:44 -0500

    added navbar and first card content/ updated footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import MainCard from "./component/MainCard";
 
 
 function App() {
   return (
     <>
       <Header />
-      <div>Hello</div>
+      <MainCard/>
       <Footer />
     </>
   );
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,8 +1,22 @@
 function Footer() {
     return (
       <>
+
         <footer>
-          Footer Tab!
+        <div class="wrapper" id="neon" >
+          gmail
+    <div class="main-div main-div1">
+      <h1>Layne</h1>
+    </div>
+    <div class="main-div main-div2">
+      <h1>Nigro</h1>
+    </div>
+    <div class="main-div main-div3">
+      <h1>Portfolio</h1>
+    </div>
+    github
+  </div>
+  
         </footer>
         
   
