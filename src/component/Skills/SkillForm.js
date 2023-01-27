58ad8208f6b72d3340667c487462e246179c8ef6
layne nigro <laynenigro@gmail.com>
2023-01-27 12:08:14 -0500

    added send email logic and line breaks

--- src/component/Skills/SkillForm.js
+++ src/component/Skills/SkillForm.js
@@ -3,6 +3,40 @@ import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Button from '@mui/material/Button';
+import emailjs from '@emailjs/browser';
+
+// const btn = document.getElementById('btn');
+
+// document.getElementById('form')
+//  .addEventListener('submit', function(event) {
+//    event.preventDefault();
+
+//    btn.value = 'Sending...';
+
+//    const serviceID = 'service_fvjmqkr';
+//    const templateID = 'template_ner8j1u';
+
+//    emailjs.sendForm(serviceID, templateID, this, 'Zr22jefdTB8t3hG2u')
+//     .then(() => {
+//       btn.value = 'Send Email';
+//       alert('Sent!');
+//     }, (err) => {
+//       btn.value = 'Send Email';
+//       alert(JSON.stringify(err));
+//     });
+// });
+
+
+
+const haiku = "HTML \n  \n CSS \n \n JavaScript \n  \n NodeJs \n \n MySql";
+const haiku2 = "React \n  \n NoSql \n \n JavaScript \n  \n NodeJs \n \n MySql";
+function replaceWithBr() {
+  return haiku.replace(/\n/g, "<br />")
+}
+
+function replaceWithBr2() {
+  return haiku2.replace(/\n/g, "<br />")
+}
 
 function SkillForm() {
   return (
@@ -35,12 +69,14 @@ function SkillForm() {
             {" "}
             <h1>Skills</h1>
           </Typography>
-          <Typography className="skillText" color="White" marginBottom={0}>
-            <h3>NodeJS</h3>
-            <h3>NodeJS</h3>
-            <h3>NodeJS</h3>
-            <h3>NodeJS</h3>
-            <h3>NodeJS</h3>
+          <Typography display={"flex"} margin="5" className="skillText" color="White" marginBottom={0}  
+          sx={{
+            padding: 5
+          }}>
+
+            <h3 dangerouslySetInnerHTML={{__html: replaceWithBr()}} class="skillText" />
+            <h3 dangerouslySetInnerHTML={{__html: replaceWithBr2()}}  class="skillText" />
+
           </Typography>
         </Box>
         <Box
@@ -91,12 +127,20 @@ function SkillForm() {
               label="Message"
               type="Search field"
             />
-          <Button className='cardBtns' size="small">Send Email</Button>
+          <Button className='cardBtns' id="btn" size="small">Send Email</Button>
           </div>
         </Box>
       </div>
+      <script type="text/javascript"
+  src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
+
+<script type="text/javascript">
+  emailjs.init('Zr22jefdTB8t3hG2u')
+</script>
     </>
   );
 }
+
+
 
 export default SkillForm;
