9fcead47b68e32547bc709f29eb8506f1b94a9f6
layne nigro <laynenigro@gmail.com>
2023-02-14 19:44:26 -0500

    updated fonts

--- src/component/MainCard.js
+++ src/component/MainCard.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import About from './About';
 import ProjectContainer from './Project/index';
+
 // import bodyImage from '../images/marek-piwnicki-RsfF900AX2A-unsplash.jpg'
 function MainCard() {
 
@@ -10,7 +11,10 @@ function MainCard() {
   
       <About/>
       <ProjectContainer/>
- 
+      <style>
+@import url('https://fonts.googleapis.com/css2?family=Euphoria+Script&family=Pinyon+Script&family=Style+Script&display=swap');
+</style>
+
 
       </>
     );
